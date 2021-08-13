3f4e490185f174cba287d664dc7c042afbd76df0
Amin Babar <amin.babar1@gmail.com>
2021-08-13 16:42:54 -0400

    can return possible moves on a board by the current player

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import boardState from "./scripts/boardState"
+// import { stat } from "fs";
+import BoardState from "./scripts/boardState"
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    let state = new boardState();
+    let state = new BoardState();
 // state.test(1, 1);
 // state.test(7, 7);
 // console.log("before");
+    // debugger;   
+    // state.printBoard();
+    // console.log(state.availableMoves());
+    state.testMove(state.availableMoves());
     state.printBoard();
+
+
 
 // console.log("clone")
 // let newState = clone(state);
--- src/scripts/boardState.js
+++ src/scripts/boardState.js
@@ -1,19 +1,23 @@
 
-class boardState {
+class BoardState {
+    
     constructor() {
-        this.board = boardState.createBoard();
+        this.board = BoardState.createBoard();
         // black is the first player in othello
         this.currentPlayer = "black";
+        this.BOARDWIDTH = 8;
+        this.BOARDLENGTH = 8;
     }
+    
 
     // A static method that would return a board that is set up!
     static createBoard() {
-        let boardWidth = 8;
-        let boardLength = 8;
+        let BOARDLENGTH = 8;
+        let BOARDWIDTH = 8;
         let board = [];
-        for (let i = 0; i < boardLength; i++) {
+        for (let i = 0; i < BOARDLENGTH; i++) {
             let row = [];
-            for (let j = 0; j < boardWidth; j++) {
+            for (let j = 0; j < BOARDWIDTH; j++) {
                 row.push(null);
             }
             board.push(row);
@@ -28,6 +32,11 @@ class boardState {
         return board;
     }
 
+    static oppColor(color) {
+        if (color === "black") return "white";
+        return "black";
+    }
+
     static nextPlayerColor(currentPlayerColor) {
         if (currentPlayerColor === white) return "black";
         return "white"
@@ -36,25 +45,100 @@ class boardState {
 
 }
 
-boardState.prototype.makeMove = function() {
+
+
+
+BoardState.prototype.makeMove = function() {
 
 }
 
-boardState.prototype.availableMoves = function () {
+// Returns all the moves for the current player 
+BoardState.prototype.availableMoves = function () {
+    // debugger;
+    let moves = [];
+    for (let i = 0; i < this.BOARDLENGTH; i++) {
+        for (let j = 0; j < this.BOARDWIDTH; j++) {
+            // console.log([i, j]);
+            if (this.validMove([i, j], this.currentPlayer)) {
+                moves.push([i, j]);
+            }
+        }
+    }
+    return moves;
+}
+
+// returns true if the current position is within bounds, else false
+BoardState.prototype.validPos = function(pos) {
+    let posR = pos[0];
+    let posC = pos[1];
+    if (posR > 7 || posR < 0 || posC > 7 || posC < 0) {
+        return false;
+    }
+    return true;
+}
+
+
+// returns true if a valid move can be made at the given position by the given
+// player
+BoardState.prototype.validMove = function(pos, player) {
+    if (!this.validPos(pos)) return false;
+
+    let dirs = [1, 0, -1];
+
+    let posR = pos[0];
+    let posC = pos[1];
+    // console.log("hey");
+    for (const dr of dirs) {
+        // console.log(dr)
+        for (const dc of dirs) {
+            // console.log([dr, dc]);
+            if (dr == 0 && dc == 0) continue;
+            
+            if (this.movesInDir(posR, dr,  posC, dc)) return true
+        }
+    }
+    return false;
+}
+
+
+// Returns true if a piece of the current players color is flanking pieces of another
+// player in a given direciton.
+BoardState.prototype.movesInDir = function(r, dr, c, dc, positions=[]) {
+    if (!this.validPos([r + dr, c + dc])) return false;
+
+    let currPiece = this.board[r + dr][c + dc];
+
+    if (!currPiece) return false;
+
+    if (currPiece === this.currentPlayer && positions.length > 0) {
+        return true;
+    }
+
+    if (currPiece === BoardState.oppColor(this.currentPlayer)) {
+        positions.push([r + dr, c + dc]);
+        return this.movesInDir(r + dr, dr, c + dc, dc, positions)
+    }
+}
+
+
+
+
+BoardState.prototype.currentPlayerScore = function () {
 
 }
 
 
-boardState.prototype.currentPlayerScore = function () {
-
+BoardState.prototype.testMove = function(arr) {
+    for (let ele of arr) {
+        this.board[ele[0]][ele[1]] = "MOVE";
+    }
 }
-
 
 
 // prints out the string version of the board to the console
-boardState.prototype.printBoard = function() {
+BoardState.prototype.printBoard = function() {
     for (let i = 0; i < this.board.length; i++) {
-        let str = "";
+        let str = `${i} `;
         for (let j = 0; j < this.board[0].length; j++) {
             if (!this.board[i][j]) {
                 str += " null  ";
@@ -69,9 +153,11 @@ boardState.prototype.printBoard = function() {
     }
 }
 
+window.BoardState = BoardState;
 
 
-export default boardState;
+
+export default BoardState;
 
 
 // class Test {
