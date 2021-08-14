54586105e09e7614924bf8b9afa15b708e1cb299
Amin Babar <amin.babar1@gmail.com>
2021-08-13 22:50:23 -0400

    added styling for the board

--- src/scripts/othelloView.js
+++ src/scripts/othelloView.js
@@ -1,22 +1,21 @@
 
 
-
 class View {
     constructor(game, el) {
         this.game = game;
         this.el = el;
-        // debugger;
         let ul = this.setupBoard();
         this.el.appendChild(ul);
     }
 
     setupBoard() {
         const ul = document.createElement("ul");
-        // debugger;
+        ul.classList.add("board-ul")
         for (let r = 0; r < 8; r++) {
             for (let c = 0; c < 8; c++) {
                 let li = document.createElement("li");
                 li.dataset.r = r;
+                li.dataset.c = c;
                 ul.appendChild(li);
             }
         }
