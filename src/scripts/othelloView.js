516731492e746c7170d6a8fbe657f29f0d153a30
Amin Babar <amin.babar1@gmail.com>
2021-08-13 23:49:14 -0400

    styling for pieces added

--- src/scripts/othelloView.js
+++ src/scripts/othelloView.js
@@ -8,14 +8,21 @@ class View {
         this.el.appendChild(ul);
     }
 
+
     setupBoard() {
         const ul = document.createElement("ul");
         ul.classList.add("board-ul")
         for (let r = 0; r < 8; r++) {
             for (let c = 0; c < 8; c++) {
+                // each li would represent one square
                 let li = document.createElement("li");
+
+                // the piece element
+                let div = document.createElement("div");
+                div.classList.add("white");
                 li.dataset.r = r;
                 li.dataset.c = c;
+                li.appendChild(div);
                 ul.appendChild(li);
             }
         }
